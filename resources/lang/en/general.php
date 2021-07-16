f7e15dcc87d8452910e16037f71322ec593018b0
MostafaAlii <mostafa0alii@gmail.com>
2021-07-16 03:32:33 +0200

    finish user creating

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -1,4 +1,10 @@
 <?php
 return [
     'system_name'               =>          'Techno POS',
+    // Alerts
+    'sorry'                     =>          'Sorry !',
+    'some_error_happining'      =>          'Some error happining try again later',
+    'done'                      =>          'Done',
+    'successfly_operation'      =>          'Successfully Operation',
+    'your_data_creating_successfully'   =>  'Your Data Creating Successfully'
 ];
